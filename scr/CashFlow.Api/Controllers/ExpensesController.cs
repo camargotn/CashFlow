26f33d71fcb1fb25d8cc6785315d9b08ee839b0e
Thiago Camargo <thiagocamargo@alumni.usp.br>
2025-01-28 23:18:41 -0300

    feat: filter exception

--- scr/CashFlow.Api/Controllers/ExpensesController.cs
+++ scr/CashFlow.Api/Controllers/ExpensesController.cs
@@ -1,7 +1,5 @@
 ﻿using CashFlow.Application.UseCases.Expense.Register;
 using CashFlow.Communication.Requests;
-using CashFlow.Communication.Responses;
-using CashFlow.Exception.ExceptionBase;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CashFlow.Api.Controllers;
@@ -13,23 +11,8 @@ public class ExpensesController : ControllerBase
     [HttpPost]
     public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
     {
-        try
-        {
-            var response = new RegisterExpenseUseCase().Execute(request);
+        var response = new RegisterExpenseUseCase().Execute(request);
 
-            return Created(string.Empty, response);
-        }
-        catch (ErrorOnValidationException ex)
-        {
-            var errorResponse = new ResponseErrorJson(ex.Errors);
-
-            return BadRequest(errorResponse);
-        }
-        catch
-        {
-            var errorResponse = new ResponseErrorJson("Unknown Error");
-
-            return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
-        }
+        return Created(string.Empty, response);
     }
 }
